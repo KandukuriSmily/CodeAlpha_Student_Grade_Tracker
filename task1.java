f282d0f609feed9f0210be80098d3fef257e3f9d
KandukuriSmily <166635330+KandukuriSmily@users.noreply.github.com>
2024-04-10 21:03:38 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class task1{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("enter no of students data you want to post:");
+        int n = sc.nextInt();
+        //for(int i=0;i<n;i++){
+            System.out.println("enter student grade:");
+            for(int i=0;i<n;i++){
+             list.add(sc.nextInt());
+        }
+        int high=0,sum=0,low=1000;
+        for(int i=0;i<list.size();i++){
+           if(list.get(i)>high)
+           high=list.get(i);
+
+           if(list.get(i)<low)
+           low=list.get(i);
+           sum+=list.get(i);
+
+        }
+        System.out.println("highest score:"+high);
+        System.out.println("lowest score:"+low);
+        System.out.println("average score:"+(sum/n));
+        sc.close();
+           
+    }
+}
